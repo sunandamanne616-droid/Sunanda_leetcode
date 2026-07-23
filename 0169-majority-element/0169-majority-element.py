1a09dce2a3edbadd55ea661935d890462be68075
SUNANDA MANNE <sunandamanne616@gmail.com>
2026-07-23 21:33:05 +0530

    Time: 0 ms (100%), Space: 13.7 MB (38.85%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,10 +1,15 @@
-class Solution(object):
-    def majorityElement(self, arr):
-        maj=len(arr)//2
-        freq={}
-        for num in arr:
-            freq[num]=freq.get(num,0)+1
-        for k,v in freq.items():
-            if v>maj:
-                return k
-        
+class Solution:
+    def majorityElement(self, nums):
+        candidate = None
+        count = 0
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+
+        return candidate
